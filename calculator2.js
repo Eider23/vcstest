353474309d71cf9e7c15c25e2d7ad0f72c6a0f98
Eider <99924915+Eider23@users.noreply.github.com>
2022-04-19 20:03:44 -0500

    19/04

--- calculator2.js
+++ calculator2.js
@@ -10,6 +10,21 @@
 
 //Divide
 
+// abs
+function absoluto(a) { 
+    showResult('el valor absoluto de  ('+a+')' +'es',Math.abs(a));
+
+  }
+
+//pow
+function pow(a,b){
+  showResult('su resultado base: '+ a + ', exponente: '+ b +':', Math.pow(a,b))
+}   
+
+//max
+function max(a,b){
+    showResult('Max(' + a + ',' + b + ')', Math.max(a,b))
+}
 
 //Helper functions
 function showResult(operation, result) {
